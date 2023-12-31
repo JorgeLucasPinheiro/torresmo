69c9ec14bec6f49ce522f3d954a3f6b9054b303b
Pinheiro <105086256+JorgeLucasPinheiro@users.noreply.github.com>
2023-12-31 06:22:29 -0300

    update

--- torresmoweb/app/components/helpers.tsx
+++ torresmoweb/app/components/helpers.tsx
@@ -2,46 +2,40 @@ import Image from "next/image"
 
 export function Nav () {
     return (
-        <div className="flex items-center justify-center bg-slate-700 mx-auto" >
-            <a href="/" className="p-3 hover:bg-slate-600">Inicio</a>
-            <a href="/" className="p-3 hover:bg-slate-600">Produtos</a>
-            <a href="/" className="p-3 hover:bg-slate-600">Sobre</a>
-            <a href="/" className="p-3 hover:bg-slate-600">Contato</a>
-            <a href="/" className="p-3 hover:bg-slate-600">Localização</a>
+        <div className="flex items-center justify-center bg-orange-800 mx-auto" >
+            <a href="/" className="p-3 hover:bg-orange-600">Inicio</a>
+            <a href="/" className="p-3 hover:bg-orange-600">Produtos</a>
+            <a href="/" className="p-3 hover:bg-orange-600">Sobre</a>
+            <a href="/" className="p-3 hover:bg-orange-600">Contato</a>
+            <a href="/" className="p-3 hover:bg-orange-600">Localização</a>
         </div>
     )
 }
 
-export function Products () {
+function SingleProduts({name, price}) {
     return (
         <div className=" md:flex justify-center" id="Produtos">
-            <div className="flex flex-col items-center bg-slate-800 m-1 md:my-2 rounded-md">
-                <Image className="rounded-lg mx-1"
+            <div className="flex flex-col items-center bg-orange-700 m-1 md:my-2 rounded-md">
+                <Image className="rounded-lg m-1 mt-3 md:mt-1"
                     src='/banha.jpg'
                     width={300}
                     height={200}
                     alt="Foto do torresmo"
                 />
-                <p className="text-center m-5">Mandioca</p>
+                <p className="text-center m-5 text-4xl">{name}</p>
+                <p className="p-2 text-3xl">{price}</p>
+                <button className="bg-orange-600 hover:bg-orange-800 p-3 m-3 rounded-3xl text-2xl" >Comprar</button>
             </div>
-            <div className="flex flex-col items-center bg-slate-800 m-1 md:my-2 rounded-md">
-                <Image className="rounded-lg mx-1"
-                    src='/banha.jpg'
-                    width={300}
-                    height={200}
-                    alt="Foto do torresmo"
-                />
-                <p className="text-center m-5">Mandioca</p>
-            </div>
-            <div className="flex flex-col items-center bg-slate-800 m-1 md:my-2 rounded-md">
-                <Image className="rounded-lg mx-1"
-                    src='/banha.jpg'
-                    width={300}
-                    height={200}
-                    alt="Foto do torresmo"
-                />
-                <p className="text-center m-5">Mandioca</p>
-            </div>
+        </div>
+    )
+}
+
+export function Products() {
+    return (
+        <div className=" md:flex justify-center" id="Produtos">
+            <SingleProduts name="Banha suina" price="R$ 29,99"/>
+            <SingleProduts name="Torresmo" price="R$ 39,99"/>
+            <SingleProduts name="Mandioca" price="R$ 5,99"/>
         </div>
     )
 }
--- torresmoweb/app/page.tsx
+++ torresmoweb/app/page.tsx
@@ -2,8 +2,8 @@ import { Nav , Products } from "./components/helpers"
 
 export default function Home() {
   return (
-    <>
+    <div className="bg-orange-400">
       <Nav/>
       <Products/>
-    </>
+    </div>
   )}
